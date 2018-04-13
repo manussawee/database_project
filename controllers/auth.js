73e74e1663aed94290896ef87fe3b84e1342816f
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-04-13 21:38:40 +0700

    getPayment added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('../config/mysql');
+let express = require('express');
+let router = express.Router();
+let mysql = require('../config/mysql');
 
 router.get('/', function (req, res, next) {
 	mysql.query("SELECT * FROM students", function (err, result) {
@@ -10,10 +10,10 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/student', function(req,res){
-	var student_id = req.body.student_id;
-	var password = req.body.password;
-	var is_found = false;
-	var is_grad = false;
+	let student_id = req.body.student_id;
+	let password = req.body.password;
+	let is_found = false;
+	let is_grad = false;
 	const query = `SELECT * FROM students\
 	WHERE student_id = ${student_id} AND password = ${password};`;
 	mysql.query(query, function(err, result, fields){
@@ -24,7 +24,7 @@ router.post('/student', function(req,res){
 		req.session.userID = student_id;
 		is_found = true;
 		// return res.send('FOUND');
-		var query2 = `SELECT * FROM students A RIGHT JOIN grad_students B\
+		let query2 = `SELECT * FROM students A RIGHT JOIN grad_students B\
 		  ON A.student_id = B.student_id  WHERE B.student_id = ${student_id}`;
 		mysql.query(query2, function(err,result,fields){
 		  if(err) throw err;
@@ -34,7 +34,7 @@ router.post('/student', function(req,res){
 			  return res.send(result);
 			}
 			if(!is_grad){
-			  var query3 = `SELECT * FROM students A RIGHT JOIN undergrad_students B\
+			  let query3 = `SELECT * FROM students A RIGHT JOIN undergrad_students B\
 			  ON A.student_id = B.student_id  WHERE B.student_id = ${student_id}`;
 			  mysql.query(query3,function(err,result,fields){
 				if(err) throw err;
@@ -51,7 +51,7 @@ router.post('/student', function(req,res){
 });
 
 router.post('/instructor', function (req, res) {
-	var sql = "SELECT * FROM instructors WHERE instructor_id = ? and password = ?";
+	let sql = "SELECT * FROM instructors WHERE instructor_id = ? and password = ?";
 	mysql.query(sql, [req.body.instructor_id, req.body.password], function (err, result) {
 		if (err) res.send({});
 		else {
@@ -73,11 +73,11 @@ router.get('/check', function (req, res) {
 	console.log(req.session.userType);
 	if (req.session.isLogin) {
 		if (req.session.userType == 'student') {
-			var sql = "SELECT * FROM students WHERE student_id = ?";
+			let sql = "SELECT * FROM students WHERE student_id = ?";
 		}
 		else if (req.session.userType == 'instructor') {
 			console.log("OK")
-			var sql = "SELECT * FROM instructors WHERE instructor_id = ?";
+			let sql = "SELECT * FROM instructors WHERE instructor_id = ?";
 		}
 		mysql.query(sql, [req.session.userID], function (err, result) {
 			if (err) res.send({});
--- controllers/student.js
+++ controllers/student.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('../config/mysql');
+let express = require('express');
+let router = express.Router();
+let mysql = require('../config/mysql');
 
 router.get('/', function (req, res, next) {
   mysql.query("SELECT * FROM students", function (err, result) {
@@ -9,13 +9,36 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.get('/payment', function(req, res,next){
+  let student_id = req.session.userID.toString();
+  let query = `SELECT * FROM undergrad_students WHERE student_id=${req.session.userID }`;
+  mysql.query(query, function (err, result1){
+    if(err || result1.length == 0) return res.send('ERROR');
+    else{
+      query = `SELECT * FROM students WHERE student_id=${req.session.userID }`;
+      mysql.query(query, function (err, result2){
+        if(err) return res.send('ERROR');
+        else{
+          let date  = new Date().getFullYear();
+          query = `SELECT * FROM fees WHERE year=${date} AND department_id=${result2[0].department_id}\
+           AND faculty_id=${result2[0].faculty_id}`;
+          mysql.query(query, function(err, result3){
+            if(err) return res.send('ERROR');
+            else return res.send(result3);
+          });
+        }
+      });
+    }
+  });
+});
+
 router.post('/register/withdraw', function(req, res, next){
-  var course_id = req.body.course_id;
-  var section_id = req.body.section_id;
-  var year = req.body.year;
-  var semester = req.body.semester;
-  var student_id = req.session.userID;
-  var query = `UPDATE register SET grade = 'W' WHERE course_id=${course_id} AND section_id=${section_id}\
+  let course_id = req.body.course_id;
+  let section_id = req.body.section_id;
+  let year = req.body.year;
+  let semester = req.body.semester;
+  let student_id = req.session.userID;
+  let query = `UPDATE register SET grade = 'W' WHERE course_id=${course_id} AND section_id=${section_id}\
   AND year=${year} AND semester=${semester} AND student_id=${student_id}`;
   mysql.query(query, function(err, result){
     if(err) return res.send('FAIL');
@@ -26,20 +49,20 @@ router.post('/register/withdraw', function(req, res, next){
 });
 
 router.post('/register/remove', function(req, res, next){
-  var course_id = req.body.course_id;
-  var section_id = req.body.section_id;
-  var year = req.body.year;
-  var semester = req.body.semester;
-  var student_id = req.session.userID;
+  let course_id = req.body.course_id;
+  let section_id = req.body.section_id;
+  let year = req.body.year;
+  let semester = req.body.semester;
+  let student_id = req.session.userID;
   console.log(student_id);
-  var query1 = `SELECT * FROM undergrad_students WHERE student_id = ${student_id}`;
+  let query1 = `SELECT * FROM undergrad_students WHERE student_id = ${student_id}`;
   mysql.query(query1, function(err, result1){
     if(err){
       console.log('NOT IN UNDERGRAD STUDENTS');
       return res.send('FAIL');
     }
     else{
-      var query2 = `DELETE FROM database_project.register WHERE course_id=${course_id} \
+      let query2 = `DELETE FROM database_project.register WHERE course_id=${course_id} \
       AND section_id=${section_id} AND year=${year} AND semester=${semester} AND student_id=${student_id};`;
       mysql.query(query2, function(err, result2){
         if(err){
