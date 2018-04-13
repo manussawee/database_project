2db87a4f5d691c0cba3981cb3978c0699d1dcb45
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-04-13 21:51:52 +0700

    grade added

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -9,4 +9,15 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.post('/course/grade', function(req, res, next){
+  let body = req.body;
+  let query = `UPDATE register SET grade=${body.grade} WHERE course_id=${body.course_id}\
+  AND year=${body.year} AND semester=${body.semester} AND student_id=${body.student_id}
+  AND section_id=${body.section_id}`;
+  mysql.query(query, function(err, result){
+    if(err) return res.send('FAIL');
+    else return res.send('OK');
+  });
+});
+
 module.exports = router;
