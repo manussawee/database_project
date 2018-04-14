58ae849f1edf8502941e80e02b8c740bfef83970
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-04-14 16:09:28 +0700

    getStudent

--- controllers/course.js
+++ controllers/course.js
@@ -88,4 +88,49 @@ router.get('/all', function (req, res, next) {
 
 });
 
+router.get('/section/student', function(req, res, next){
+	req.session.isLogin = true;
+	if(!req.session.isLogin) res.send('FAIL');
+	else{
+		let student_info = [];
+		let query = `SELECT student_id FROM register WHERE course_id=${req.query.course_id} \
+		AND section_id=${req.query.section_id} AND year=${req.query.year} AND semester=${req.query.semester}`
+		mysql.query(query, function(err, result1){
+			if(err) return err;
+			else{
+				const promises = result1.map(result => {
+					return new Promise((resolve, reject) => {
+						query = `SELECT * FROM students A RIGHT JOIN grad_students B\
+						ON A.student_id = B.student_id  WHERE B.student_id = ${result.student_id}`;
+						mysql.query(query, function(err,grad_student,fields){
+							if(err) reject(err);
+							else{
+								if(grad_student.length != 0){
+									delete grad_student[0].password;
+									student_info.push(grad_student[0]);
+									resolve(grad_student)
+								}
+								else{
+									console.log('part3')
+									query = `SELECT * FROM students A RIGHT JOIN undergrad_students B\
+									ON A.student_id = B.student_id  WHERE B.student_id = ${result.student_id}`;
+									mysql.query(query,function(err,ungrad_student,fields){
+									if(err) reject(err);
+									else{
+										delete ungrad_student[0].password;
+										student_info.push(ungrad_student[0]);
+										resolve(ungrad_student);
+									}
+									})
+								}
+							}
+						});
+					});
+				});
+				Promise.all(promises).then(()=>res.send(student_info));
+			}
+		});
+	}
+});
+
 module.exports = router;
--- controllers/student.js
+++ controllers/student.js
@@ -9,70 +9,90 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/payment', function(req, res,next){
-  let student_id = req.session.userID.toString();
-  let query = `SELECT * FROM undergrad_students WHERE student_id=${req.session.userID }`;
-  mysql.query(query, function (err, result1){
-    if(err || result1.length == 0) return res.send('ERROR');
-    else{
-      query = `SELECT * FROM students WHERE student_id=${req.session.userID }`;
-      mysql.query(query, function (err, result2){
-        if(err) return res.send('ERROR');
-        else{
-          let date  = new Date().getFullYear();
-          query = `SELECT * FROM fees WHERE year=${date} AND department_id=${result2[0].department_id}\
-           AND faculty_id=${result2[0].faculty_id}`;
-          mysql.query(query, function(err, result3){
-            if(err) return res.send('ERROR');
-            else return res.send(result3);
-          });
-        }
-      });
-    }
-  });
+router.post('/request', function(req, res, next){
+  if(!req.session.isLogin) res.send('FAIL');
+  else{
+    let query = `INSERT INTO requests (student_id, type) VALUES (${req.session.userID.toString()}, '${req.body.type}')`
+    mysql.query(query, function(err, result){
+      if(err) return res.send(err);
+      else return res.send('OK');
+    });
+  }
+});
+
+router.get('/payment', function(req, res, next){
+  if(!req.session.isLogin) res.send('FAIL');
+  else{
+    let student_id = req.session.userID.toString();
+    let query = `SELECT * FROM undergrad_students WHERE student_id=${req.session.userID }`;
+    mysql.query(query, function (err, result1){
+      if(err || result1.length == 0) return res.send('ERROR');
+      else{
+        query = `SELECT * FROM students WHERE student_id=${req.session.userID }`;
+        mysql.query(query, function (err, result2){
+          if(err) return res.send('ERROR');
+          else{
+            let date  = new Date().getFullYear();
+            query = `SELECT * FROM fees WHERE year=${date} AND department_id=${result2[0].department_id}\
+            AND faculty_id=${result2[0].faculty_id}`;
+            mysql.query(query, function(err, result3){
+              if(err) return res.send('ERROR');
+              else return res.send(result3);
+            });
+          }
+        });
+      }
+    });
+  }
 });
 
 router.post('/register/withdraw', function(req, res, next){
-  let course_id = req.body.course_id;
-  let section_id = req.body.section_id;
-  let year = req.body.year;
-  let semester = req.body.semester;
-  let student_id = req.session.userID;
-  let query = `UPDATE register SET grade = 'W' WHERE course_id=${course_id} AND section_id=${section_id}\
-  AND year=${year} AND semester=${semester} AND student_id=${student_id}`;
-  mysql.query(query, function(err, result){
-    if(err) return res.send('FAIL');
-    else{
-      return res.send('OK')
-    }
-  });
+  if(!req.session.isLogin) res.send('FAIL');
+  else{
+    let course_id = req.body.course_id;
+    let section_id = req.body.section_id;
+    let year = req.body.year;
+    let semester = req.body.semester;
+    let student_id = req.session.userID;
+    let query = `UPDATE register SET grade = 'W' WHERE course_id=${course_id} AND section_id=${section_id}\
+    AND year=${year} AND semester=${semester} AND student_id=${student_id}`;
+    mysql.query(query, function(err, result){
+      if(err) return res.send('FAIL');
+      else{
+        return res.send('OK')
+      }
+    });
+  }
 });
 
 router.post('/register/remove', function(req, res, next){
-  let course_id = req.body.course_id;
-  let section_id = req.body.section_id;
-  let year = req.body.year;
-  let semester = req.body.semester;
-  let student_id = req.session.userID;
-  console.log(student_id);
-  let query1 = `SELECT * FROM undergrad_students WHERE student_id = ${student_id}`;
-  mysql.query(query1, function(err, result1){
-    if(err){
-      console.log('NOT IN UNDERGRAD STUDENTS');
-      return res.send('FAIL');
-    }
-    else{
-      let query2 = `DELETE FROM database_project.register WHERE course_id=${course_id} \
-      AND section_id=${section_id} AND year=${year} AND semester=${semester} AND student_id=${student_id};`;
-      mysql.query(query2, function(err, result2){
-        if(err){
-          console.log('UNABLE TO REMOVE REGISTERED COURSE');
-          return res.send('FAIL');
-        }
-        else return res.send('OK');
-      });
-    }
-  });
+  if(!req.session.isLogin) res.send('FAIL');
+  else{
+    let course_id = req.body.course_id;
+    let section_id = req.body.section_id;
+    let year = req.body.year;
+    let semester = req.body.semester;
+    let student_id = req.session.userID;
+    console.log(student_id);
+    let query1 = `SELECT * FROM undergrad_students WHERE student_id = ${student_id}`;
+    mysql.query(query1, function(err, result1){
+      if(err){
+        console.log('NOT IN UNDERGRAD STUDENTS');
+        return res.send('FAIL');
+      }
+      else{
+        let query2 = `DELETE FROM database_project.register WHERE course_id=${course_id} \
+        AND section_id=${section_id} AND year=${year} AND semester=${semester} AND student_id=${student_id};`;
+        mysql.query(query2, function(err, result2){
+          if(err){
+            console.log('UNABLE TO REMOVE REGISTERED COURSE');
+            return res.send('FAIL');
+          }
+          else return res.send('OK');
+        });
+      }
+    });
+  }
 });
 
 module.exports = router;
