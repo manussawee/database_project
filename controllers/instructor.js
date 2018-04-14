0be590599f0dc0e1622fe24c7aef5f7b2d1cec0e
cuviengchai <gmchawit@gmail.com>
2018-04-14 10:35:03 +0700

    add advisees

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -9,4 +9,33 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.get('/advisees',function(req,res){
+  if(req.session.userType === 'instructor'){
+    let userID = req.session.userID;
+    let sql = `SELECT S.student_id, course_id, year,semester, grade \
+              FROM students S INNER JOIN register R ON S.student_id = R.student_id
+              WHERE advisor_id = ${userID};`
+    let result = [];
+    console.log(userID);
+    mysql.query(sql,function(err,students){
+      console.log(students);
+      if (err) res.send({});
+      else{
+        students.map((student,index) =>{
+          result.push(student);
+          console.log(students.length-1);
+          if(index === students.length-1) {
+            console.log("OK3");
+            res.send({'students' : result});
+          }
+        });
+      }
+    });
+  }
+  else res.send({});
+});
+
+// Result: 
+// {students: students (array of object)}
+
 module.exports = router;
